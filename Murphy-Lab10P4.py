715a5e0839a0de4326b45b5a285f87da53915c25
smurphy2230 <smurphy2230@gmail.com>
2020-04-13 08:36:37 -0400

    lab complete

--- Murphy-Lab10P4.py
+++ Murphy-Lab10P4.py
@@ -3,17 +3,20 @@ input_file = open("water.txt", "r")
 for line in input_file:
     line = line.strip("\n")
     line_items = line.split(" ")
-    if line_items[1] == "R":
-        if float(line_items[2]) <= 6000:
-            cost_water = float(line_items[2]) * .005
+    account_number = line_items[0]
+    account_type = line_items[1]
+    water_used = float(line_items[2])
+    if account_type == "R":
+        if water_used <= 6000:
+            cost_water = water_used * .005
         else:
-            cost_water = (6000 * .005) + (float(line_items[2]) - 6000) * .007
+            cost_water = (6000 * .005) + water_used - 6000 * .007
     else:
-        if float(line_items[2]) <= 8000:
-            cost_water = float(line_items[2]) * .006
+        if water_used <= 8000:
+            cost_water = water_used * .006
         else:
-            cost_water = (8000 * .006) + (float(line_items[2]) - 8000) * .008
+            cost_water = (8000 * .006) + water_used - 8000 * .008
     print("Account number: " +
-          line_items[0] + " " + "Water charge: " + str(cost_water))
+          account_number + " " + "Water charge: " + str(cost_water))
 
 input_file.close()
